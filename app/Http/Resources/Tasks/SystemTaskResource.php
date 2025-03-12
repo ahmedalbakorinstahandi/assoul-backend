28406e9441995a24d025b8d8d9017a8c596c8a3b
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-03-12 13:23:35 +0300

    fix: correct typo in image column name and add system task completion to resource

--- app/Http/Resources/Tasks/SystemTaskResource.php
+++ app/Http/Resources/Tasks/SystemTaskResource.php
@@ -23,6 +23,7 @@ class SystemTaskResource extends JsonResource
             'unique_key' => $this->unique_key,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'system_task_completion' => SystemTaskCompletionResource::collection($this->whenLoaded('systemTaskCompletion')),
         ];
     }
 }

d0554347d7cd91a4000ec9917c9320666b119396
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-03-16 20:00:01 +0300

    fix: update Patient UpdateRequest to allow nullable fields

--- app/Http/Requests/Users/Patient/UpdateRequest.php
+++ app/Http/Requests/Users/Patient/UpdateRequest.php
@@ -10,15 +10,15 @@ class UpdateRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            'user.first_name' => 'required|string|max:255',
-            'user.last_name' => 'required|string|max:255',
-            'user.avatar' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'gender' => 'required|string|in:male,female,other',
-            'birth_date' => 'required|date',
-            'height' => 'required|numeric|min:0',
-            'weight' => 'required|numeric|min:0',
-            'diabetes_diagnosis_age' => 'required|integer|min:0',
-            'insulin_doses' => 'required|integer|min:1',
+            'user.first_name' => 'nullable|string|max:255',
+            'user.last_name' => 'nullable|string|max:255',
+            'user.avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'gender' => 'nullable|string|in:male,female,other',
+            'birth_date' => 'nullable|date',
+            'height' => 'nullable|numeric|min:0',
+            'weight' => 'nullable|numeric|min:0',
+            'diabetes_diagnosis_age' => 'nullable|integer|min:0',
+            'insulin_doses' => 'nullable|integer|min:1',
         ];
     }
 }

959aea90d03b60cc1082fafb3d9b994b30688ff3
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-03-17 21:29:49 +0300

    fix: update avatar validation to require image type and increase file size limit

--- app/Http/Requests/Users/Doctor/UpdateProfileRequest.php
+++ app/Http/Requests/Users/Doctor/UpdateProfileRequest.php
@@ -14,7 +14,7 @@ class UpdateProfileRequest extends BaseFormRequest
             'user.last_name' => 'nullable|string|max:255',
             'user.phone' => 'nullable|string|max:255',
             'user.password' => 'nullable|string|min:6|confirmed',
-            'user.avatar' => 'nullable|string|max:110',
+            'user.avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'specialization' => 'nullable|string|max:255',
             'classification_number' => 'nullable|string|max:255',
             'workplace_clinic_location' => 'nullable|string|max:255',

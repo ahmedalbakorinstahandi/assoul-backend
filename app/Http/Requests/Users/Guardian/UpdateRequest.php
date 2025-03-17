e90f6e553e04ed4b150b1f8e086695e1a1c5790c
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-03-18 00:01:29 +0300

    fix: update avatar validation to accept string with max length of 110

--- app/Http/Requests/Users/Guardian/UpdateRequest.php
+++ app/Http/Requests/Users/Guardian/UpdateRequest.php
@@ -12,7 +12,7 @@ class UpdateRequest extends BaseFormRequest
         return [
             'user.first_name' => 'nullable|string|max:255',
             'user.last_name' => 'nullable|string|max:255',
-            'user.avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'user.avatar' => 'nullable|string|max:110',
             'user.phone' => 'nullable|string|max:255',
             'user.password' => 'nullable|string|min:6|confirmed',
         ];

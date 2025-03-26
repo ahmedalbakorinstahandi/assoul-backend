a17ca18de16121cdf7baf061bc9b0f46497a3965
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-03-26 14:05:47 +0300

    fix: update follow request validation to conditionally require doctor_id based on user role

--- app/Http/Requests/Patients/DoctorPatient/FollowRequest.php
+++ app/Http/Requests/Patients/DoctorPatient/FollowRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Patients\DoctorPatient;
 
 use App\Http\Requests\BaseFormRequest;
+use App\Models\Users\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class FollowRequest extends BaseFormRequest
@@ -10,7 +11,7 @@ class FollowRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
-            'doctor_id' => 'required|exists:users,id,deleted_at,NULL',
+            'doctor_id' => !User::auth()->isAdmin()  ? '' :  'required|exists:users,id,deleted_at,NULL',
             'patient_id' => 'required|exists:users,id,deleted_at,NULL',
         ];
     }

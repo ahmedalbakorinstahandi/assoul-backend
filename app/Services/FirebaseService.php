6594df5c40528cafa82f531fd17e14d5cafef507
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-04-12 21:35:48 +0300

    fix: update Firebase service account path to include .json extension

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -238,7 +238,7 @@ class FirebaseService
      */
     protected static function loadServiceAccount()
     {
-        $serviceAccountPath = storage_path('firebase/assoul-70249-firebase-adminsdk-fbsvc-8be1e8af96');
+        $serviceAccountPath = storage_path('firebase/assoul-70249-firebase-adminsdk-fbsvc-8be1e8af96.json');
 
         if (!file_exists($serviceAccountPath)) {
             throw new \Exception("Firebase service account file not found.");

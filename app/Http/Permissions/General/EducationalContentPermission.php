2e0bbec99845bc741c08d4c6ec41428a45ef7ca8
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-06-12 17:51:26 +0300

    feat: implement visibility check for educational content based on user role

--- app/Http/Permissions/General/EducationalContentPermission.php
+++ app/Http/Permissions/General/EducationalContentPermission.php
@@ -3,11 +3,19 @@
 namespace App\Http\Permissions\General;
 
 use App\Models\General\EducationalContent;
+use App\Models\Users\User;
 
 class EducationalContentPermission
 {
     public static function index($query)
     {
+
+        $user = User::auth();
+
+        if (!$user->isAdmin()) {
+            return $query->where('is_visible', true);
+        }
+
         return $query;
     }
 
